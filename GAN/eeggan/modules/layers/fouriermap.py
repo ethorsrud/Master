caa696e534463ffa4fbba50e3bd8e23bbc904c7a
ethorsrud <eirik.thorsrud@fys.uio.no>
2019-10-22 13:56:48 +0200

    Minor changes

--- GAN/eeggan/modules/layers/fouriermap.py
+++ GAN/eeggan/modules/layers/fouriermap.py
@@ -8,4 +8,6 @@ class FFTMap1d(nn.Module):
         super(FFTMap1d,self).__init__()
 
     def forward(self,input):
+        fft = torch.rfft(input,1,normalized=True)
+        fft = torch.sqrt(fft[:,:,1:,0]**2+fft[:,:,1:,1]**2)
         return input
