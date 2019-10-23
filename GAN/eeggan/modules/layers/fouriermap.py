d27f7f642a22e9fbcbf2bbbf73048ee8205cc7f2
Eirik Thorsrud <eirikthorsrud@1x-193-157-196-123.uio.no>
2019-10-23 14:53:29 +0200

    Minor change

--- GAN/eeggan/modules/layers/fouriermap.py
+++ GAN/eeggan/modules/layers/fouriermap.py
@@ -12,6 +12,7 @@ class FFTMap1d(nn.Module):
         fft = torch.rfft(input,1,normalized=True)
         fft = torch.sqrt(fft[:,:,1:,0]**2+fft[:,:,1:,1]**2)
         upsampler = torch.nn.Upsample(scale_factor=2,mode='linear')
-        input = torch.cat((input,upsampler(fft)),dim=1)
+        print(upsampler(fft).shape)
+        #input = torch.cat((input,upsampler(fft)),dim=1)
         print("THIS IS THE OUTPUT SHAPE\n",input.shape)
         return input
