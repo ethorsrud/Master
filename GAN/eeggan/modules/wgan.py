e0d84220a66edb32c257d4514e832d53b37527b2
ethorsrud <eirik.thorsrud@fys.uio.no>
2019-11-04 14:45:09 +0100

    No log

--- GAN/eeggan/modules/wgan.py
+++ GAN/eeggan/modules/wgan.py
@@ -373,7 +373,7 @@ class WGAN_I_Generator(GAN_Generator):
 		gen = self(batch_noise)
 		fft = torch.transpose(torch.rfft(torch.transpose(gen,2,3),1,normalized=False),2,3)
 		fft = torch.sqrt(fft[:,:,1:,:,0]**2+fft[:,:,1:,:,1]**2)#fft[:,:,:,:,0]**2
-		fft = torch.log(fft+1)
+		#fft = torch.log(fft)
 		fft = ((fft-MSM[0])/MSM[1])/MSM[2]
 		#fft = torch.mean(fft,dim=0).view(1,fft.shape[1],fft.shape[2],fft.shape[3])
 		#autocor = functions.autocorrelation(gen)
