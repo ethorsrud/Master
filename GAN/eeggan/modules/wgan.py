0f47dcae79b32e4e7ad873daa744cb22f3dfbb88
ethorsrud <eirik.thorsrud@fys.uio.no>
2019-11-06 13:03:02 +0100

    Loss-balance

--- GAN/eeggan/modules/wgan.py
+++ GAN/eeggan/modules/wgan.py
@@ -386,7 +386,7 @@ class WGAN_I_Generator(GAN_Generator):
 		loss2 = disc2.mean()
 		#loss3 = disc3.mean()
 
-		loss = loss+loss2#+loss3e
+		loss = 0.8*loss+0.2*loss2#+loss3e
 		# Backprop gradient
 		loss.backward(mone)
 
