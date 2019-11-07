3f13004e6c3299c9ebfaabf55b85f95f0ea40764
ethorsrud <eirik.thorsrud@fys.uio.no>
2019-11-07 14:35:58 +0100

    test

--- GAN/eeggan/examples/conv_lin/run.py
+++ GAN/eeggan/examples/conv_lin/run.py
@@ -320,6 +320,8 @@ for i_block in range(i_block_tmp,n_blocks):
             yf = np.abs(fft(batch_fake.transpose(0,1,3,2)).transpose(0,1,3,2))
             freqs = fftpack.fftfreq(input_length)*sf
             mask = freqs>=0
+            print(yf.shape)
+            print((yf.tranpose(3,0,1,2)).shape)
             yf = (yf.transpose(3,0,1,2)[mask]).transpose(1,2,3,0)
             freqs = freqs[mask]
             f,Pxx_den = signal.welch(batch_fake.transpose(0,1,3,2),sf,nperseg=1024)
