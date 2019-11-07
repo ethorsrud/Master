cf31b6d5f74b53b5c26f18c443f49cf42e4368af
ethorsrud <eirik.thorsrud@fys.uio.no>
2019-11-07 21:54:25 +0100

    Readd foruier disc

--- GAN/eeggan/modules/wgan.py
+++ GAN/eeggan/modules/wgan.py
@@ -386,7 +386,7 @@ class WGAN_I_Generator(GAN_Generator):
 		loss2 = disc2.mean()
 		#loss3 = disc3.mean()
 
-		loss = (loss+loss2)/2.0#0.2*loss2#+loss3e
+		loss = (loss+loss2)/2.0
 		# Backprop gradient
 		loss.backward(mone)
 
