Fixed std on plot + fixed normalization for batch

GAN/eeggan/examples/conv_lin/run.py
@@ -33,7 +33,7 @@ os.environ['CUDA_LAUNCH_BLOCKING'] = '1'
 torch.backends.cudnn.enabled=True
 torch.backends.cudnn.benchmark=True
 
-torch.cuda.set_device(0)
+torch.cuda.set_device(3)
 
 n_critic = 5
 n_batch = 56#64
@@ -158,14 +158,14 @@ for i_block in range(i_block_tmp,n_blocks):
     train_tmp = discriminator.model.downsample_to_block(Variable(torch.from_numpy(train).cuda(),requires_grad=False),discriminator.model.cur_block).data.cpu()
     #train_tmp_fft = fourier_discriminator.model.downsample_to_block(Variable(torch.from_numpy(fft_train).cuda(),requires_grad=False),fourier_discriminator.model.cur_block).data.cpu()
     train_tmp_fft = torch.tensor(np.abs(np.fft.rfft(train_tmp,axis=2)))#torch.tensor(np.real(np.fft.rfft(train_tmp,axis=2))**2)
-    train_tmp_fft = train_tmp_fft[:,:,1:,:]
-    train_tmp_fft = torch.log(train_tmp_fft)
+    #train_tmp_fft = train_tmp_fft[:,:,1:,:]
+    #train_tmp_fft = torch.log(train_tmp_fft)
     #train_mean = torch.mean(train_tmp,(0,2)).squeeze().cuda()
     #train_std = torch.std(torch.std(train_tmp,0),1).squeeze().cuda()
-    fft_mean = torch.mean(train_tmp_fft,(0,2)).squeeze().cuda()
-    fft_std = torch.std(torch.std(train_tmp_fft,0),1).squeeze().cuda()
-    fft_max = torch.max(torch.max(torch.abs(train_tmp_fft),0)[0],1)[0].squeeze().cuda()
-    print("MEAN",fft_mean,"STD",fft_std,"MAX",fft_max)
+    #fft_mean = torch.mean(train_tmp_fft,(0,2)).squeeze().cuda()
+    #fft_std = torch.std(torch.std(train_tmp_fft,0),1).squeeze().cuda()
+    #fft_max = torch.max(torch.max(torch.abs(train_tmp_fft),0)[0],1)[0].squeeze().cuda()
+    #print("MEAN",fft_mean,"STD",fft_std,"MAX",fft_max)
 
 
 
@@ -189,27 +189,31 @@ for i_block in range(i_block_tmp,n_blocks):
                 z_vars = rng.normal(0,1,size=(len(batches[it*n_critic+i_critic]),n_z)).astype(np.float32)
                 z_vars = Variable(torch.from_numpy(z_vars),requires_grad=False).cuda()
                 batch_fake = Variable(generator(z_vars).data,requires_grad=True).cuda()
+
+                batch_real_fft = torch.transpose(torch.rfft(torch.transpose(batch_real,2,3),1,normalized=False),2,3)
+                batch_real_fft = torch.sqrt(batch_real_fft[:,:,1:,:,0]**2+batch_real_fft[:,:,1:,:,1]**2)#batch_real_fft[:,:,:,:,0]**2
+                batch_fake_fft = torch.transpose(torch.rfft(torch.transpose(batch_fake,2,3),1,normalized=False),2,3)
+                batch_fake_fft = torch.sqrt(batch_fake_fft[:,:,1:,:,0]**2+batch_fake_fft[:,:,1:,:,1]**2)#batch_fake_fft[:,:,:,:,0]**2
+                
+                #batch_fake_fft = torch.log(batch_fake_fft)
+                #batch_real_fft = torch.log(batch_real_fft)
+
+                fake_mean = torch.mean(batch_fake_fft,(0,2)).squeeze()
+                fake_std = torch.std(torch.std(batch_fake_fft,0),1).squeeze()
+                real_mean = torch.mean(batch_real_fft,(0,2)).squeeze()
+                real_std = torch.std(torch.std(batch_real_fft,0),1).squeeze()
+
+                batch_fake_fft = ((batch_fake_fft-fake_mean)/fake_std)#/fft_max
+                batch_real_fft = ((batch_real_fft-real_mean)/real_std)#/fft_max
+
+                #batch_fake_fft = torch.mean(batch_fake_fft,dim=0).view(1,batch_fake_fft.shape[1],batch_fake_fft.shape[2],batch_fake_fft.shape[3])
+                #batch_real_fft = torch.mean(batch_real_fft,dim=0).view(1,batch_real_fft.shape[1],batch_real_fft.shape[2],batch_real_fft.shape[3])
                 """
                 plt.plot(batch_real[0,0,:,0].cpu().detach().numpy(),label="real")
                 plt.plot(batch_fake[0,0,:,0].cpu().detach().numpy(),label="fake")
                 plt.legend()
                 plt.show()
                 """
-                batch_real_fft = torch.transpose(torch.rfft(torch.transpose(batch_real,2,3),1,normalized=False),2,3)
-                batch_real_fft = torch.sqrt(batch_real_fft[:,:,1:,:,0]**2+batch_real_fft[:,:,1:,:,1]**2)#batch_real_fft[:,:,:,:,0]**2
-                batch_fake_fft = torch.transpose(torch.rfft(torch.transpose(batch_fake,2,3),1,normalized=False),2,3)
-                batch_fake_fft = torch.sqrt(batch_fake_fft[:,:,1:,:,0]**2+batch_fake_fft[:,:,1:,:,1]**2)#batch_fake_fft[:,:,:,:,0]**2
-                
-  
-                batch_fake_fft = torch.log(batch_fake_fft)
-                batch_real_fft = torch.log(batch_real_fft)
-
-                batch_fake_fft = ((batch_fake_fft-fft_mean)/fft_std)#/fft_max
-                batch_real_fft = ((batch_real_fft-fft_mean)/fft_std)#/fft_max
-
-                #batch_fake_fft = torch.mean(batch_fake_fft,dim=0).view(1,batch_fake_fft.shape[1],batch_fake_fft.shape[2],batch_fake_fft.shape[3])
-                #batch_real_fft = torch.mean(batch_real_fft,dim=0).view(1,batch_real_fft.shape[1],batch_real_fft.shape[2],batch_real_fft.shape[3])
-
                 """
                 plt.figure()
                 plt.plot(batch_fake_fft[0,0,:,0].cpu().detach().numpy())
@@ -234,7 +238,7 @@ for i_block in range(i_block_tmp,n_blocks):
             
             z_vars = rng.normal(0,1,size=(n_batch,n_z)).astype(np.float32)
             z_vars = Variable(torch.from_numpy(z_vars),requires_grad=True).cuda()
-            loss_g = generator.train_batch(z_vars,discriminator,fourier_discriminator,AC_discriminator,[fft_mean,fft_std,fft_max])
+            loss_g = generator.train_batch(z_vars,discriminator,fourier_discriminator,AC_discriminator)
 
         losses_d.append(loss_d)
         losses_g.append(loss_g)
@@ -283,8 +287,8 @@ for i_block in range(i_block_tmp,n_blocks):
             
             for channel_i in range(fake_amps.shape[1]):
                 plt.figure()
-                log_std_fake = np.std(np.log(torch_fake_fft.data.cpu().numpy()),axis=0).squeeze()
-                log_std_real = np.std(np.log(train_fft),axis=0).squeeze()
+                log_std_fake = np.log(np.std(torch_fake_fft.data.cpu().numpy(),axis=0)).squeeze()
+                log_std_real = np.log(np.std(train_fft,axis=0)).squeeze()
                 logmin = np.min(np.log(train_amps[:,channel_i]))
                 logmax = np.max(np.log(train_amps[:,channel_i]))
                 plt.ylim(logmin-np.abs(logmax-logmin)*0.15,logmax+np.abs(logmax-logmin)*0.15)

GAN/eeggan/modules/wgan.py
@@ -346,7 +346,7 @@ class WGAN_I_Generator(GAN_Generator):
 		self.optimizer = optim.Adam(self.parameters(),lr=alpha,betas=betas)
 		self.did_init_train = True
 
-	def train_batch(self, batch_noise, discriminator1,discriminator2,discriminator3,MSM):
+	def train_batch(self, batch_noise, discriminator1,discriminator2,discriminator3):
 		"""
 		Train generator for one batch of latent noise
 
@@ -373,8 +373,11 @@ class WGAN_I_Generator(GAN_Generator):
 		gen = self(batch_noise)
 		fft = torch.transpose(torch.rfft(torch.transpose(gen,2,3),1,normalized=False),2,3)
 		fft = torch.sqrt(fft[:,:,1:,:,0]**2+fft[:,:,1:,:,1]**2)#fft[:,:,:,:,0]**2
-		fft = torch.log(fft)
-		fft = ((fft-MSM[0])/MSM[1])#/MSM[2]
+		#fft = torch.log(fft)
+		fft_mean = torch.mean(fft,(0,2)).squeeze()
+		fft_std = torch.std(torch.std(fft,0),1).squeeze()
+		fft = (fft-fft_mean)/fft_std
+		#fft = ((fft-MSM[0])/MSM[1])#/MSM[2]
 		#fft = torch.mean(fft,dim=0).view(1,fft.shape[1],fft.shape[2],fft.shape[3])
 		#autocor = functions.autocorrelation(gen)
 		
@@ -385,7 +388,7 @@ class WGAN_I_Generator(GAN_Generator):
 		loss = disc.mean()
 		loss2 = disc2.mean()
 		#loss3 = disc3.mean()
-		#print("loss:",loss,"Loss2:",loss2)
+		print("loss:",loss,"Loss2:",loss2)
 		loss = (loss+loss2)/2.0
 		# Backprop gradient
 		loss.backward(mone)