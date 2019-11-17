ac52bd6eb4b0c886fbd246d27768436ed8be7652
ethorsrud <eirik.thorsrud@fys.uio.no>
2019-11-17 20:11:29 +0100

    debug

--- GAN/eeggan/modules/wgan.py
+++ GAN/eeggan/modules/wgan.py
@@ -394,49 +394,54 @@ class WGAN_I_Generator(GAN_Generator):
 		self.pre_train(discriminator1)
 		self.pre_train(discriminator2)
 		#self.pre_train(discriminator3)
-		with autograd.detect_anomaly():
-			mone = torch.FloatTensor([1]) * -1
-			batch_noise,mone = utils.cuda_check([batch_noise,mone])
-			# Generate and discriminate
-			gen = self(batch_noise)
-			fft = torch.transpose(torch.rfft(torch.transpose(gen,2,3),1,normalized=False),2,3)
-			fft = torch.sqrt(fft[:,:,1:,:,0]**2+fft[:,:,1:,:,1]**2)#fft[:,:,:,:,0]**2
-			
-			#fft = torch.log(fft)
+		#with autograd.detect_anomaly():
+		mone = torch.FloatTensor([1]) * -1
+		batch_noise,mone = utils.cuda_check([batch_noise,mone])
+		# Generate and discriminate
+		gen = self(batch_noise)
+		
+		fft = torch.transpose(torch.rfft(torch.transpose(gen,2,3),1,normalized=False),2,3)
 
-			fft_mean = torch.mean(fft,(0,2)).squeeze()
-			fft_std = torch.sqrt(torch.mean((fft-fft_mean)**2,dim=(0,1,2)))
-			#NORMALIZING OVER BATCH ONLY
-			#fft_mean = torch.mean(fft,(0)).squeeze()
-			#fft_std = torch.std(fft,0).squeeze()
-			fft = (fft-fft_mean)/fft_std
-			#fft = ((fft-MSM[0])/MSM[1])#/MSM[2]
-			#fft = torch.mean(fft,dim=0).view(1,fft.shape[1],fft.shape[2],fft.shape[3])
-			#autocor = functions.autocorrelation(gen)
-			
-			disc = discriminator1(gen)
-			disc2 = discriminator2(fft)
-			#disc3 = discriminator3(autocor)
+		fft_for_invest = fft.data.cpu().numpy()
+		print("finite 1?",np.all(np.isfinite(fft_for_invest)))
+		fft = torch.sqrt(fft[:,:,1:,:,0]**2+fft[:,:,1:,:,1]**2)#fft[:,:,:,:,0]**2
+		fft_for_invest = fft.data.cpu().numpy()
+		print("finite 2?",np.all(np.isfinite(fft_for_invest)))
+		#fft = torch.log(fft)
 
-			loss = disc.mean()
-			loss2 = disc2.mean()
-			#loss3 = disc3.mean()
-			#print("loss:",loss,"Loss2:",loss2)
-			loss = (loss+loss2)/2.0
-			#print("GENLOSS",loss)
-			# Backprop gradient
-			loss.backward(mone)
-			# Update parameters
-			"""
-			for p in self.parameters():
-				print("MAX param:",np.max(np.abs(p.detach().cpu().numpy())),"MIN param:",np.min(np.abs(p.detach().cpu().numpy())))
-				try:
-					print("MAX param_grad",np.max(np.abs(p.grad.data.cpu().numpy())),"MIN param_grad",np.min(np.abs(p.grad.data.cpu().numpy())))
-				except:
-					continue
-			"""		
+		fft_mean = torch.mean(fft,(0,2)).squeeze()
+		fft_std = torch.sqrt(torch.mean((fft-fft_mean)**2,dim=(0,1,2)))
+		#NORMALIZING OVER BATCH ONLY
+		#fft_mean = torch.mean(fft,(0)).squeeze()
+		#fft_std = torch.std(fft,0).squeeze()
+		fft = (fft-fft_mean)/fft_std
+		#fft = ((fft-MSM[0])/MSM[1])#/MSM[2]
+		#fft = torch.mean(fft,dim=0).view(1,fft.shape[1],fft.shape[2],fft.shape[3])
+		#autocor = functions.autocorrelation(gen)
+		
+		disc = discriminator1(gen)
+		disc2 = discriminator2(fft)
+		#disc3 = discriminator3(autocor)
 
-			self.update_parameters()
+		loss = disc.mean()
+		loss2 = disc2.mean()
+		#loss3 = disc3.mean()
+		#print("loss:",loss,"Loss2:",loss2)
+		loss = (loss+loss2)/2.0
+		#print("GENLOSS",loss)
+		# Backprop gradient
+		loss.backward(mone)
+		# Update parameters
+		"""
+		for p in self.parameters():
+			print("MAX param:",np.max(np.abs(p.detach().cpu().numpy())),"MIN param:",np.min(np.abs(p.detach().cpu().numpy())))
+			try:
+				print("MAX param_grad",np.max(np.abs(p.grad.data.cpu().numpy())),"MIN param_grad",np.min(np.abs(p.grad.data.cpu().numpy())))
+			except:
+				continue
+		"""		
+
+		self.update_parameters()
 
 		loss = loss.data.item()
 		return loss # return loss
