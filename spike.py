8d309ea1ce50c965b1acf8133b9dcdc370f540e7
ethorsrud <eirik.thorsrud@fys.uio.no>
2019-12-04 16:10:55 +0100

    testing paths

--- spike.py
+++ spike.py
@@ -14,5 +14,5 @@ hp_filtered = False
 data = np.memmap(dat_path, dtype, "r", offset, (n_channels_dat, data_len//n_channels_dat))
 spike_times = np.load(os.path.normpath(kilosort_path+os.sep+"spike_times.npy")).astype(np.uint64)
 spike_templates = np.load(os.path.normpath(kilosort_path+os.sep+"spike_templates.npy")).astype(np.uint32)
-print(spike.times.shape)
+print(spike_times.shape)
 print(spike_templates.shape)
