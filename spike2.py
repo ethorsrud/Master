a9199bd76f0f4ad981e660193fd47a303dfedd20
ethorsrud <eirik.thorsrud@fys.uio.no>
2019-12-16 14:38:24 +0100

    test

--- spike2.py
+++ spike2.py
@@ -22,12 +22,12 @@ amplitudes = np.load(os.path.normpath(kilosort_path+os.sep+"amplitudes.npy")).as
 channel_map = np.load(os.path.normpath(kilosort_path+os.sep+"channel_map.npy")).astype(np.int32)
 
 
-seconds_of_data = 50
+seconds_of_data = 100
 small_n_channels = 384
 times = spike_times_small = spike_times[spike_times<(seconds_of_data*sample_rate)]
 labels = spike_templates[:len(times)]
 
-offset=150
+offset=0
 small_data = data[offset:(offset+small_n_channels),:seconds_of_data*sample_rate]
 print("Small data loaded")
 small_data = small_data.astype(np.float32)
