8ae2c1edfb01f4c2a65abbd3da5df4f3ce2a55ef
ethorsrud <eirik.thorsrud@fys.uio.no>
2019-12-16 14:09:30 +0100

    test

--- spike2.py
+++ spike2.py
@@ -40,9 +40,9 @@ plt.close()
 """
 geom = np.zeros((small_n_channels,2))
 geom[:,0] = range(small_n_channels)
-recording = se.NumpyRecordingExtractor(timeseries=small_data,geom=geom,sampling_frequency=sample_rate)
+recording = se.NumpyRecordingExtractor(timeseries=data,geom=geom,sampling_frequency=sample_rate)
 #recording = st.preprocessing.bandpass_filter(recording,freq_min=300,freq_max=6000)
-recording = st.preprocessing.resample(st.preprocessing.rectify(recording), 2000)
+recording = st.preprocessing.resample(st.preprocessing.rectify(recording), 1000)
 #recording = st.preprocessing.resample(recording, 1000)
 #small_data = small_data.get_traces()
 #recording = se.NumpyRecordingExtractor(timeseries=small_data,geom=geom,sampling_frequency=sample_rate)
@@ -62,7 +62,7 @@ print('Num. events for unit 2 = {}'.format(len(st_print)))
 st2_print = sorting.get_unit_spike_train(unit_id=2, start_frame=0, end_frame=4*30000)
 print('Num. events for first four seconds of unit 2 = {}'.format(len(st2_print)))
 
-wf = st.postprocessing.get_unit_waveforms(recording, sorting, 
+wf = st.postprocessing.get_unit_waveforms(recording, sorting, ms_before=1, ms_after=2,
                                           save_as_features=True, verbose=True)
 
 print(sorting.get_shared_unit_spike_feature_names())
