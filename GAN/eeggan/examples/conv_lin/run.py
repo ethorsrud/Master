b658f3394af914be624a0a075d92b5634d1df646
ethorsrud <eirik.thorsrud@fys.uio.no>
2020-01-10 13:48:28 +0100

    Testing conditional GAN

--- GAN/eeggan/examples/conv_lin/run.py
+++ GAN/eeggan/examples/conv_lin/run.py
@@ -243,9 +243,6 @@ for i_block in range(i_block_tmp,n_blocks):
 
                 z_vars = Variable(torch.from_numpy(z_vars),requires_grad=False).cuda()
                 batch_fake = Variable(generator(z_vars).data,requires_grad=True).cuda()
-                print(batch_fake.shape)
-                print(batch_real.shape)
-                quit()
 
                 batch_real_fft = torch.transpose(torch.rfft(torch.transpose(batch_real,2,3),1,normalized=False),2,3)
                 batch_real_fft = torch.sqrt(batch_real_fft[:,:,1:,:,0]**2+batch_real_fft[:,:,1:,:,1]**2)#batch_real_fft[:,:,:,:,0]**2
