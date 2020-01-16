2a22916aa30b357bc830b09888bbd2d08dc34283
ethorsrud <eirik.thorsrud@fys.uio.no>
2020-01-16 23:35:28 +0100

    Readded linear first layer

--- GAN/eeggan/examples/conv_lin/model.py
+++ GAN/eeggan/examples/conv_lin/model.py
@@ -123,13 +123,10 @@ def create_gen_blocks(n_chans,z_vars):
 		return upsample_layer(mode='bilinear',scale_factor=(2,1))
 	blocks = []
 	#originally n_featuremaps*12
-	"""
+
 	#Original No reshape, only one linear layer z_vars,base*featuremaps
 	tmp_block = ProgressiveGeneratorBlock(
-								nn.Sequential(Reshape([[0],1,2*base]),weight_scale(nn.Linear(z_vars*2,base),
-														gain=calculate_gain('leaky_relu')),
-												nn.LeakyReLU(0.2),
-								weight_scale(nn.Linear(base,n_featuremaps*base),
+								nn.Sequential(Reshape([[0],1,2*base]),weight_scale(nn.Linear(z_vars*2,base*n_featuremaps),
 														gain=calculate_gain('leaky_relu')),
 												nn.LeakyReLU(0.2),
 												Reshape([[0],n_featuremaps,-1]),
@@ -138,8 +135,8 @@ def create_gen_blocks(n_chans,z_vars):
 								create_fade_sequence(2)
 								)
 	
+
 	"""
-	
 	tmp_block = ProgressiveGeneratorBlock(
 								nn.Sequential(Reshape([[0],2,-1]),
 								weight_scale(nn.Conv1d(2,n_featuremaps,21,padding=10),
@@ -157,7 +154,7 @@ def create_gen_blocks(n_chans,z_vars):
 								create_out_sequence(n_chans,n_featuremaps),
 								create_fade_sequence(2)
 								)
-	
+	"""
 	blocks.append(tmp_block)
 	tmp_block = ProgressiveGeneratorBlock(
 								create_conv_sequence(n_featuremaps+1,n_featuremaps),
