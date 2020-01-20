2a68c864821138df14051602794236a77cb2ce95
ethorsrud <eirik.thorsrud@fys.uio.no>
2020-01-20 14:40:33 +0100

    Fixed labels getting downsampled

--- GAN/eeggan/modules/progressive.py
+++ GAN/eeggan/modules/progressive.py
@@ -45,7 +45,7 @@ class ProgressiveDiscriminator(nn.Module):
 			#orig_label = input[:,:,:,-1]
 			#orig_label_np = orig_label.cpu().detach().numpy()
 			#print(np.where(orig_label_np==1.))
-			print("hey")
+			pass
 
 		for i in range(self.cur_block,len(self.blocks)):
 			if alpha<1. and i==self.cur_block:
