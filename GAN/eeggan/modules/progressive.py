a9f773a929e53c13dc6fce13ccf7aac94d2ab10f
ethorsrud <eirik.thorsrud@fys.uio.no>
2020-01-22 14:27:09 +0100

    conditional enable/disable

--- GAN/eeggan/modules/progressive.py
+++ GAN/eeggan/modules/progressive.py
@@ -135,21 +135,21 @@ class ProgressiveGenerator(nn.Module):
 		for i in range(0,self.cur_block+1):
 			#Adding labels to input
 			if self.conditional:
-				labels = np.zeros(shape=(input.shape[0],base*2**i))
-				label_downsampled = np.floor(label/(2**(n_blocks-i))).astype(np.int)
-				indexes = (np.arange(input.shape[0]).astype(np.int),label_downsampled)
-				labels[indexes] = 1.
-				if i==0:
-					input = input[:,:,None]
-					labels = labels[:,:,np.newaxis].astype(np.float32)
-					labels = torch.from_numpy(labels).cuda()
-					#print("i==0",input.shape,labels.shape)
-					input = torch.cat((input,labels),dim=2)
-				else:
-					labels = labels[:,np.newaxis,:].astype(np.float32)
-					labels = torch.from_numpy(labels).cuda()
-					#print("i!=0",input.shape,labels.shape)
-					input = torch.cat((input,labels),dim=1)
+			labels = np.zeros(shape=(input.shape[0],base*2**i))
+			label_downsampled = np.floor(label/(2**(n_blocks-i))).astype(np.int)
+			indexes = (np.arange(input.shape[0]).astype(np.int),label_downsampled)
+			labels[indexes] = 1.
+			if i==0:
+				input = input[:,:,None]
+				labels = labels[:,:,np.newaxis].astype(np.float32)
+				labels = torch.from_numpy(labels).cuda()
+				#print("i==0",input.shape,labels.shape)
+				input = torch.cat((input,labels),dim=2)
+			elif i!=0 and self.conditional:
+				labels = labels[:,np.newaxis,:].astype(np.float32)
+				labels = torch.from_numpy(labels).cuda()
+				#print("i!=0",input.shape,labels.shape)
+				input = torch.cat((input,labels),dim=1)
 
 			
 
