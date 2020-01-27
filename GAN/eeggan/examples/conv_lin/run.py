3def4b2e53480d611757aabc506352b136880b3f
ethorsrud <eirik.thorsrud@fys.uio.no>
2020-01-27 13:00:58 +0100

    Testing reshape

--- GAN/eeggan/examples/conv_lin/run.py
+++ GAN/eeggan/examples/conv_lin/run.py
@@ -63,24 +63,23 @@ sample_rate = 30000
 hp_filtered = False
 
 spike_data = np.memmap(dat_path, dtype, "r", offset, (n_channels_dat, data_len//n_channels_dat))
-spike_data_small = spike_data[:50,:input_length*n_samples].T
+spike_data_small = spike_data[:3,:input_length*n_samples].T
 print(spike_data_small.shape)
-
+spike_data_test = spike_data_small.reshape((n_samples,1,input_length,3))
+print(spike_data_test[0,0,:,0])
 train_new = []
 for i in range(int(spike_data_small.shape[0]/input_length)):
     print("%i/%i"%(i,int(spike_data_small.shape[0]/input_length)))
     train_new.append(spike_data_small[i*input_length:i*input_length+input_length])
 print("creating array")
 train_new = np.array(train_new)
-print("new axis")
 train = train_new[:,:,:,np.newaxis]
-print("swap1")
 train = np.swapaxes(train,1,2)
-print("swap2")
 train = np.swapaxes(train,1,3)
 #Only first channel
 #train = train[:,:,:,0][:,:,:,np.newaxis]
 n_chans = train.shape[3]#+1 # +1 FOR CONDITIONAL
+print(train[0,0,:,0])
 print("Number of channels:",n_chans)
 print(train.shape)
 #Spike data end
