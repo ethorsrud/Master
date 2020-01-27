9f188543ca341970461616c4a907ce8b929d28e1
ethorsrud <eirik.thorsrud@fys.uio.no>
2020-01-27 14:09:02 +0100

    Prints

--- spike2.py
+++ spike2.py
@@ -22,6 +22,8 @@ amplitudes = np.load(os.path.normpath(kilosort_path+os.sep+"amplitudes.npy")).as
 channel_map = np.load(os.path.normpath(kilosort_path+os.sep+"channel_map.npy")).astype(np.int32)
 
 
+print(spike_times[0:100])
+quit()
 seconds_of_data = 15
 small_n_channels = 384
 times = spike_times_small = spike_times[spike_times<(seconds_of_data*sample_rate)]
