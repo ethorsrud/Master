19bb7aa2096f06d6c5335548aef1a91ef939ad2f
ethorsrud <eirik.thorsrud@fys.uio.no>
2020-01-29 10:29:41 +0100

    More channels

--- using_saved_gan.py
+++ using_saved_gan.py
@@ -55,17 +55,18 @@ z_vars = Variable(torch.from_numpy(z_vars_im),requires_grad=False).cuda()
 
 batch_fake = generator(z_vars)
 
-template_0_extended = np.zeros(input_length)
-peak_length = templates.shape[1]
-template_0_extended[int(input_length//2):(int(input_length//2+peak_length))] = templates[0,:,0]*0.3
-for i in range(200):
-    plt.plot(np.arange(4025,4175),batch_fake[i,0,4025:4175,0].detach().cpu().numpy(),linewidth=0.3,alpha=0.5)
-plt.plot(np.arange(4025,4175),template_0_extended[4025:4175],label="Template",linewidth = 1.0,alpha=0.5)
-plt.legend()
-plt.title("100 signals where the label is set \n to create spike of template 0 in the middle")
-plt.xlabel("Sample i of 8192 total")
-plt.savefig("Block_5_MiddlePeak.png",dpi=1000)
-plt.close()
+for ch in range(3):
+    template_extended = np.zeros(input_length)
+    peak_length = templates.shape[1]
+    template_extended[int(input_length//2):(int(input_length//2+peak_length))] = templates[0,:,ch]*0.3
+    for i in range(200):
+        plt.plot(np.arange(4025,4175),batch_fake[i,0,4025:4175,ch].detach().cpu().numpy(),linewidth=0.3,alpha=0.5)
+    plt.plot(np.arange(4025,4175),template_extended[4025:4175],label="Template",linewidth = 1.0,alpha=0.5)
+    plt.legend()
+    plt.title("100 signals where the label is set \n to create spike of template 0 in the middle \n CHANNEL %i"%ch)
+    plt.xlabel("Sample i of 8192 total")
+    plt.savefig("Block_5_MiddlePeak_ch%i.png"%ch,dpi=1000)
+    plt.close()
 
 
 """
