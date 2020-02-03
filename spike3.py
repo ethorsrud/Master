ff61e62d81267475491da5577253e625f1b699a0
ethorsrud <eirik.thorsrud@fys.uio.no>
2020-02-03 15:44:59 +0100

    test

--- spike3.py
+++ spike3.py
@@ -39,8 +39,8 @@ recording = st.preprocessing.resample(st.preprocessing.rectify(recording), 1000)
 recording = st.preprocessing.common_reference(recording, reference='median')
 
 print(os.path.normpath(os.getcwd()+os.sep+os.pardir+os.sep+"Kilosort2"))
-os.environ["KILOSORT2_PATH"] = os.path.normpath(os.getcwd()+os.sep+os.pardir+os.sep+"Kilosort2"+os.sep)
-
+os.environ["KILOSORT2_PATH"] = os.path.normpath(os.getcwd()+os.sep+os.pardir+os.sep+"Kilosort2")
+print(os.getenv("KILOSORT_PATH"))
 sorting_KS2 = ss.run_kilosort2(recording, output_folder='tmp_KS2')
 #default_kilosort2_params = ss.kilosort2.default_params()
 
