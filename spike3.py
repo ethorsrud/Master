8d3ac31c7f04c043ccdfcdbc2b687d6eda4a5ea5
ethorsrud <eirik.thorsrud@fys.uio.no>
2020-02-03 15:38:37 +0100

    test

--- spike3.py
+++ spike3.py
@@ -38,8 +38,8 @@ recording = st.preprocessing.notch_filter(recording, freq=1000, q=10)
 recording = st.preprocessing.resample(st.preprocessing.rectify(recording), 1000)
 recording = st.preprocessing.common_reference(recording, reference='median')
 
-print(os.path.normpath(os.getcwd()+os.sep+os.pardir+os.sep+"Kilosort2"))
-os.environ["KILOSORT2_PATH"] = os.path.normpath(os.getcwd()+os.sep+os.pardir+os.sep+"Kilosort2")
+print(os.path.normpath(os.getcwd()+os.sep+os.pardir))
+os.environ["KILOSORT2_PATH"] = os.path.normpath(os.getcwd()+os.sep+os.pardir)
 
 sorting_KS2 = ss.run_kilosort2(recording, output_folder='tmp_KS2')
 #default_kilosort2_params = ss.kilosort2.default_params()
