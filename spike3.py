0a8ca4fa1f8a2e5ad07b19e655ffda31d4d0d581
ethorsrud <eirik.thorsrud@fys.uio.no>
2020-02-03 13:00:41 +0100

    test

--- spike3.py
+++ spike3.py
@@ -22,7 +22,7 @@ templates_ind = np.load(os.path.normpath(kilosort_path+os.sep+"templates_ind.npy
 amplitudes = np.load(os.path.normpath(kilosort_path+os.sep+"amplitudes.npy")).astype(np.double)#[nSpikes, ]
 channel_map = np.load(os.path.normpath(kilosort_path+os.sep+"channel_map.npy")).astype(np.int32)
 
-print(templates_ind[0])
+print(channel_map)
 quit()
 
 selected_template = 0
