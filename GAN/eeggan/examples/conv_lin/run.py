fac1798889f54b32550a580894789251b6ad7222
ethorsrud <eirik.thorsrud@fys.uio.no>
2020-02-04 15:10:21 +0100

    fixing labels

--- GAN/eeggan/examples/conv_lin/run.py
+++ GAN/eeggan/examples/conv_lin/run.py
@@ -163,7 +163,7 @@ time_labels = np.zeros(shape=(n_samples,1,input_length,1))
 #Only spikes with selected template
 #spike_times = spike_times[temp_index]
 #spike_times = spike_times[spike_times<(input_length*n_samples)]
-for i in range(spike_times.shape[0]):
+for i in range(spike_times.shape[0]-1):
     cur_sample = int(spike_times[i]//input_length)
     cur_ind = int(spike_times[i]%input_length)
     time_labels[cur_sample,0,cur_ind:(cur_ind+label_length),0] = 1.
