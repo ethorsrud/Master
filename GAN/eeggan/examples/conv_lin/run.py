04e59cb28d2f1ef19ecbb9ab0ce9c559a436ae1b
ethorsrud <eirik.thorsrud@fys.uio.no>
2020-02-04 21:06:22 +0100

    test

--- GAN/eeggan/examples/conv_lin/run.py
+++ GAN/eeggan/examples/conv_lin/run.py
@@ -169,10 +169,12 @@ for i in range(spike_times.shape[0]):
     time_labels[cur_sample,0,cur_ind:(cur_ind+label_length),0] = 1.
 
 n_spikes_per_channel = np.sum(time_labels,axis=2).squeeze()
+print(n_spikes_per_channel)
+
 spikes_mean = np.mean(n_spikes_per_channel)
 spikes_std = np.sqrt(np.mean((n_spikes_per_channel-spikes_mean)**2))
 print("Spikes_mean",spikes_mean,"Spikes_std",spikes_std)
-
+quit()
 train = np.concatenate((train,time_labels),axis=3).astype(np.float32)
 print("train_shape",train.shape)
 
