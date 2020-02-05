dd6ffbc51025bcb223bae60e9a217ef3a5b06d20
ethorsrud <eirik.thorsrud@fys.uio.no>
2020-02-05 12:58:30 +0100

    generating fake dataset

--- using_saved_gan.py
+++ using_saved_gan.py
@@ -58,7 +58,7 @@ for i in range(768):
 
 z_vars_im = np.concatenate((z_vars_im,labels),axis=1)
 z_vars = Variable(torch.from_numpy(z_vars_im),requires_grad=False).cuda()
-batch_fake = generator(z_vars_im)
+batch_fake = generator(z_vars)
 print(batch_fake.shape)
 
 
