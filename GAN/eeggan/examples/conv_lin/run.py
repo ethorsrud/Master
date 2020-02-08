0853379753b185eab5daf0e118fdc2d7aa1e3623
ethorsrud <eirik.thorsrud@fys.uio.no>
2020-02-08 15:58:48 +0100

    saving data

--- GAN/eeggan/examples/conv_lin/run.py
+++ GAN/eeggan/examples/conv_lin/run.py
@@ -88,7 +88,8 @@ spike_data = np.memmap(dat_path, dtype, "r", offset, (data_len//n_channels_dat,n
 spike_data_small = spike_data[:input_length*n_samples,120:180]
 train = spike_data_small.reshape((n_samples,input_length,60))[:,np.newaxis,:,:]
 #train = spike_data_small.reshape((n_samples,1,input_length,15))
-
+np.save("spike_data_ch120:ch180.npy",spike_data_small)
+quit()
 #FILTERING
 #b,a = butter(10,6000/(0.5*sample_rate),btype="low")
 #train = lfilter(b,a,train,axis=2)
