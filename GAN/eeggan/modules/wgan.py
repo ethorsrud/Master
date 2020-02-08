33415f477a26725ff74837287735bd3cdbadaab5
ethorsrud <eirik.thorsrud@fys.uio.no>
2020-02-08 19:27:04 +0100

    loss change

--- GAN/eeggan/modules/wgan.py
+++ GAN/eeggan/modules/wgan.py
@@ -464,7 +464,7 @@ class WGAN_I_Generator(GAN_Generator):
 		loss2 = disc2.mean()
 		#loss3 = disc3.mean()
 		#print("loss:",loss,"Loss2:",loss2)
-		loss = 0.2*loss2+0.8*loss#(loss+loss2)/2.0
+		loss = loss+loss2#0.2*loss2+0.8*loss#(loss+loss2)/2.0
 		#print("GENLOSS",loss)
 		# Backprop gradient
 		loss.backward(mone)
