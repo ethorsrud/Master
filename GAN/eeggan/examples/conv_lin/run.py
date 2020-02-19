e558d1a8cd1da4c63ccc4913f0e16e7e8dbeb41e
ethorsrud <eirik.thorsrud@fys.uio.no>
2020-02-19 23:45:08 +0100

    test

--- GAN/eeggan/examples/conv_lin/run.py
+++ GAN/eeggan/examples/conv_lin/run.py
@@ -87,11 +87,8 @@ hp_filtered = False
 spike_data = np.memmap(dat_path, dtype, "r", offset, (data_len//n_channels_dat,n_channels_dat))
 spike_data_small = spike_data[:input_length*n_samples,120:180]
 train = spike_data_small.reshape((n_samples,input_length,60))[:,np.newaxis,:,:]
-for i in range(29,33):
-    print(i,np.mean(train[:,0,:,i]))
-    print("max",np.max(train[:,0,:,i]))
-    print("min",np.min(train[:,0,:,i]))
-quit()
+train = np.delete(train,31,3)
+train = np.delete(train,53,3)
 #train = spike_data_small.reshape((n_samples,1,input_length,15))
 #np.save("spike_data_ch120:ch180.npy",spike_data_small)
 #quit()
