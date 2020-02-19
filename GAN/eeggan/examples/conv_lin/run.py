b7d0784619a2f5639e3e3852aa84f0182ab268f4
ethorsrud <eirik.thorsrud@fys.uio.no>
2020-02-19 16:35:22 +0100

    Increased label_length

--- GAN/eeggan/examples/conv_lin/run.py
+++ GAN/eeggan/examples/conv_lin/run.py
@@ -137,7 +137,7 @@ n_chans = train.shape[3]
 print("Number of channels:",n_chans)
 print(train.shape)
 """
-label_length = 1#80#1
+label_length = 40#1#80#1
 """
 peak = np.linspace(0,2*np.pi,80)
 peak = np.sin(peak)*200
