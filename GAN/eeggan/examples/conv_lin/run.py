8640c78590b19cf9e230ff0de5b9748b2940e48a
ethorsrud <eirik.thorsrud@fys.uio.no>
2020-02-20 12:30:07 +0100

    test

--- GAN/eeggan/examples/conv_lin/run.py
+++ GAN/eeggan/examples/conv_lin/run.py
@@ -157,7 +157,7 @@ for i in range(n_samples):
 """
 train = train-np.mean(train,axis=(0,2)).squeeze()#-train.mean()
 train = train/np.std(train,axis=(0,2)).squeeze()#train.std()
-#train = train/np.max(np.abs(train),axis=(0,2)).squeeze()#np.abs(train).max()
+train = train/np.max(np.abs(train)).squeeze()#np.max(np.abs(train),axis=(0,2)).squeeze()#np.abs(train).max()
 
 #spike_times = np.load(os.path.normpath(kilosort_path+os.sep+"spike_times.npy")).astype(np.uint64) #[nSpikes,]
 #spike_templates = np.load(os.path.normpath(kilosort_path+os.sep+"spike_templates.npy")).astype(np.uint32) #[nSpikes,]
