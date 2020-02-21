198f5b0cb70d2127d02337d7f8bc57483bd29294
ethorsrud <eirik.thorsrud@fys.uio.no>
2020-02-21 18:38:05 +0100

    test

--- GAN/eeggan/examples/conv_lin/model.py
+++ GAN/eeggan/examples/conv_lin/model.py
@@ -141,7 +141,10 @@ def create_gen_blocks(n_chans,z_vars,conditional):
 	
 	tmp_block = ProgressiveGeneratorBlock(
 								nn.Sequential(Reshape([[0],57,-1]),
-								weight_scale(nn.Conv1d(57,n_featuremaps,9,padding=4),
+								weight_scale(nn.Conv1d(57,57,9,padding=4,stride=4),
+														gain=calculate_gain('leaky_relu')),
+												nn.LeakyReLU(0.2),
+								weight_scale(nn.Conv1d(57,n_featuremaps,9,padding=4,stride=4),
 														gain=calculate_gain('leaky_relu')),
 												nn.LeakyReLU(0.2),
 								weight_scale(nn.Conv1d(n_featuremaps,n_featuremaps,9,padding=4),
