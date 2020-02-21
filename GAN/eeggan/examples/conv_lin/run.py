765e4ab7db3a4e2efafa672d47882d14bec007fa
ethorsrud <eirik.thorsrud@fys.uio.no>
2020-02-21 12:33:17 +0100

    test

--- GAN/eeggan/examples/conv_lin/run.py
+++ GAN/eeggan/examples/conv_lin/run.py
@@ -163,11 +163,12 @@ train = train/np.max(np.abs(train)).squeeze()#np.max(np.abs(train),axis=(0,2)).s
 #spike_templates = np.load(os.path.normpath(kilosort_path+os.sep+"spike_templates.npy")).astype(np.uint32) #[nSpikes,]
 #selected_template = 0
 #temp_index = np.where(spike_templates==selected_template)[0]
-spike_times = np.load("spike_times_ch220_ch260.npy").astype(np.uint64)
+spike_times = np.load("spike_times_ch120_ch180.npy").astype(np.uint64)
 spike_templates = np.load(code_path+os.sep+"spike_templates_ch120_ch180.npy").astype(np.uint32)
 templates = np.load(code_path+os.sep+"templates_ch_120_ch_180.npy").astype(np.float32)
 channelmap = np.load(os.path.normpath(kilosort_path+os.sep+"channel_map.npy")).astype(np.int32)
-
+print(templates.shape)
+quit()
 time_labels = np.zeros(shape=(n_samples,1,input_length,1))
 template_labels = np.zeros(shape=(n_samples,1,600,1))
 #Only spikes with selected template
