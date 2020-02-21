474427e5e35f13e7be5875865e4a1cfd10a76ff8
ethorsrud <eirik.thorsrud@fys.uio.no>
2020-02-21 14:32:57 +0100

    test

--- GAN/eeggan/examples/conv_lin/run.py
+++ GAN/eeggan/examples/conv_lin/run.py
@@ -176,9 +176,9 @@ conv_labels = np.zeros(shape=(n_samples,1,input_length,n_chans))
 #mask
 #spike_times = spike_times[spike_times<(input_length*n_samples)]
 
-#mask = spike_times<(input_length*n_samples)
+mask = spike_times<(input_length*n_samples)
 #mask = np.where(mask==1)
-#spike_templates = spike_templates[mask]
+spike_templates = spike_templates[mask]
 #spike_templates = spike_templates[:,0]
 
 for i in range(spike_times.shape[0]):
