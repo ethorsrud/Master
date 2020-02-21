7ac4eb59249bf3f2330425620463af9429000163
ethorsrud <eirik.thorsrud@fys.uio.no>
2020-02-21 13:56:29 +0100

    test

--- GAN/eeggan/examples/conv_lin/run.py
+++ GAN/eeggan/examples/conv_lin/run.py
@@ -185,7 +185,8 @@ for i in range(spike_times.shape[0]):
     cur_sample = int(spike_times[i]//input_length)
     cur_ind = int(spike_times[i]%input_length)
     time_labels[cur_sample,0,cur_ind:(cur_ind+label_length),0] = 1.
-    template_length = 82-cur_ind+41-input_length
+    print(cur_ind)
+    template_length = 82-((cur_ind+41)-input_length)
     print(template_length)
     conv_labels[cur_sample,0,(cur_ind-41):(cur_ind+41),:] = templates[spike_templates[i],:,:]
 print("Yeah")
