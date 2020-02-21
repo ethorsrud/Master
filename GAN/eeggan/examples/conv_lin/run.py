66af3e7e4f5e42157302f0503ac71b7e16edd509
ethorsrud <eirik.thorsrud@fys.uio.no>
2020-02-21 19:15:19 +0100

    test

--- GAN/eeggan/examples/conv_lin/run.py
+++ GAN/eeggan/examples/conv_lin/run.py
@@ -433,7 +433,7 @@ for i_block in range(i_block_tmp,n_blocks):
                 print(fake_mean.shape)
                 #fft_std = torch.sqrt(torch.mean((train_tmp_fft-fft_mean)**2,dim=(0,1,2)))
                 fake_std = torch.sqrt(torch.mean((batch_fake_fft-fake_mean)**2,dim=(0,1,2)))
-                real_mean = torch.mean(batch_real_fft,(0,2)).squeeze()#fft_mean
+                real_mean = torch.mean(batch_real_fft,(0,2)).squeeze().view(1,2,1,57)#fft_mean
                 real_std = torch.sqrt(torch.mean((batch_real_fft-real_mean)**2,dim=(0,1,2)))#fft_std
                 #NORMALIZING OVER BATCH ONLY
                 #fake_mean = torch.mean(batch_fake_fft,(0)).squeeze()
