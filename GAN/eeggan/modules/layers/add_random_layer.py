820398de2b238b807424b61b9809794a1fdcbab9
ethorsrud <eirik.thorsrud@fys.uio.no>
2020-02-26 12:27:23 +0100

    CUDA check random label

--- GAN/eeggan/modules/layers/add_random_layer.py
+++ GAN/eeggan/modules/layers/add_random_layer.py
@@ -14,4 +14,6 @@ class add_random_layer(nn.Module):
     def forward(self,input):
         normal = torch.distributions.normal.Normal(0,1, validate_args=None)
         z_vars = normal.sample(sample_shape=(input.shape))
+        if input.is_cuda:
+            z_vars = z_vars.cuda()
         return torch.cat((input,z_vars),dim=1)
