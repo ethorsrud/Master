c8e695c3f59aebc19442e7ca5bfd89cc65762eeb
ethorsrud <eirik.thorsrud@fys.uio.no>
2020-03-11 23:41:44 +0100

    test

--- GAN/eeggan/examples/conv_lin/run.py
+++ GAN/eeggan/examples/conv_lin/run.py
@@ -463,8 +463,8 @@ for i_block in range(i_block_tmp,n_blocks):
                 batch_fake_fft = torch.transpose(torch.rfft(torch.transpose(batch_fake[:,:,:,:],2,3),1,normalized=False),2,3)
                 batch_fake_fft = torch.sqrt(batch_fake_fft[:,:,:,:,0]**2+batch_fake_fft[:,:,:,:,1]**2)#batch_fake_fft[:,:,:,:,0]**2
                 
-                batch_fake_fft = torch.log(batch_fake_fft)
-                batch_real_fft = torch.log(batch_real_fft)
+                batch_fake_fft = torch.log(batch_fake_fft+0.1)
+                batch_real_fft = torch.log(batch_real_fft+0.1)
 
                 fake_mean = torch.mean(batch_fake_fft,(0,2)).squeeze()
 
